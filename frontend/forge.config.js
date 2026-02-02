15e9352078278e41c84e47e75d64a03d7638a169
sai <sanchaiavila@gmail.com>
2026-02-02 13:19:53 +0100

    Update package-lock.json with electron-builder dependencies

---
+++ frontend/forge.config.js
@@ -0,0 +1,76 @@
+const { FusesPlugin } = require("@electron-forge/plugin-fuses");
+const { FuseV1Options, FuseVersion } = require("@electron/fuses");
+
+module.exports = {
+  packagerConfig: {
+    name: "Aniways",
+    executableName: "aniways",
+    icon: "./public/Icon",
+    asar: {
+      unpack: "**/*.node"
+    },
+    ignore: [
+      /^\/node_modules\/(?!(.next|next))/,
+      /^\/\.next\/cache/,
+      /^\/\.git/,
+      /^\/src/,
+      /^\/\.env/,
+      /\.map$/,
+    ],
+    extraResource: ["./.next/standalone", "./public"],
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        name: "Aniways",
+        setupIcon: "./public/Icon.ico",
+        authors: "Aniways",
+        description: "Anime Streaming Desktop App",
+        // Creates desktop and start menu shortcuts
+        setupExe: "Aniways-Setup.exe",
+        noMsi: true,
+      },
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin", "linux", "win32"],
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      config: {
+        options: {
+          name: "aniways",
+          productName: "Aniways",
+          icon: "./public/Icon.ico",
+        },
+      },
+    },
+    {
+      name: "@electron-forge/maker-rpm",
+      config: {
+        options: {
+          name: "aniways",
+          productName: "Aniways",
+          icon: "./public/Icon.ico",
+        },
+      },
+    },
+  ],
+  plugins: [
+    {
+      name: "@electron-forge/plugin-auto-unpack-natives",
+      config: {},
+    },
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
+  ],
+};
